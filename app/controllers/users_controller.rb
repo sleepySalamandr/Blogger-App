77a2f2e2fc380ed762a82dc12ae8ae2486182b97
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-19 17:24:17 +1100

    Following feature working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ def show
  @email = @user.email
 end
 
+def follow
+  @user = User.find(params[:id])
+  current_user.followees << @user
+  redirect_back(fallback_location: user_path(@user))
+end
+
+def unfollow
+  @user = User.find(params[:id])
+  current_user.followed_users.find_by(followee_id: @user.id).destroy
+  redirect_back(fallback_location: user_path(@user))
+end
+
 end
