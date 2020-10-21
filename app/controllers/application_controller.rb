657128cf2435a6a75046d7a01e23cac06d5f9927
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-21 14:31:25 +1100

    New user info appearing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
-  # before_action :fetch_user
-  #
-  # private
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def fetch_user
     @current_user = User.find_by :id => session[:id] if session[:id].present?
     session[:id] = nil unless @current_user.present?
   end
 
-def check_for_login # checks if somebody is logged in - place in controller where you need to be logged into access
+  def check_for_login # checks if somebody is logged in - place in controller where you need to be logged into access
   redirect_to user_session_path unless current_user.present?
-end
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :blog_title, :about_me])
+  end
+  #
+  # private
+
 end
