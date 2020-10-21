3d5c9b151186bffb7409a573afebc9c243168156
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-22 09:45:45 +1100

    Css in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :blog_title, :about_me, :followee_id, :follower_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :blog_title, :about_me, :followee_id, :follower_id, :followees, :followers])
   end
   #
   # private
